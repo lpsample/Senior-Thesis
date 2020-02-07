b42772159c6f62f11bb8a7bffbb5ce9c95823ab6
Lindsey Sample <lpsample@vassar.edu>
2020-02-07 14:16:06 -0500

    Bug with list in outputs

--- NEATGridBot.R
+++ NEATGridBot.R
@@ -7,31 +7,10 @@ N.moves <-100
 N.bots <- 10
 N.gen <- 100
 
-
-run<- function(x){
-  #x<-1
-  genomes<- createGeneration(N.bots)
-  gen<- runGeneration(genomes, x)
-  
-  for (a in 2:N.gen) {
-   # print(a)
-    genomes<- next.genomes(gen, genomes)
-    for (i in 1:length(genomes)) {
-      genomes[[i]]<- mutate(genomes[[i]])
-    }
-    gen <- runGeneration(genomes, x)
-  }
-  top.scorer <- as.numeric(which.max(gen$fitness))
-  final.weights<-genomes[[top.scorer]]
-  return(final.weights)
-}
-static.run<- run(1)
-dynamic.run<- run(2)
-
 # Initial State (1 or 2 is static or dynamic, going to start with static): Initial genomes
 # STATE IN THIS VERSION IS WEIGHTS!!! EACH GEN CHANGES
-gridBot.InitialState <- function(x){
-  grids<- makeGrids(x)
+gridBot.InitialState <- function(){
+  grids<- makeGrids(1)
   obsGrid <- grids[[1]]
   lightGrid <- grids[[2]]
   state <- list(
@@ -60,8 +39,8 @@ gridBot.ConvertStateToNeuralNetInputs <- function(currentState){
 
 #Update State: Takes data of gen's run in Grid, Gives next Genome
 gridBot.UpdateState <- function(currentState,neuralNetOutputs){
-  sum.output<- sum(neuralNetOutputs)
-  adj.output <- neuralNetOutputs/sum.output
+  sum.output<- sum(unlist(neuralNetOutputs))
+  adj.output <- unlist(neuralNetOutputs)/sum.output
   
   action <- sample(c(moveForward,moveBackward, turnClock, turnCounter),1,  prob=adj.output)
   new.state <- action[[1]](currentState)
@@ -86,43 +65,22 @@ gridBot.CheckForTermination <- function(frameNum,oldState,updatedState,oldFitnes
 }
   
   #Plot
-  poleBalance.PlotState <-function(updatedState){
-    cart.centerX <- updatedState[[1]]
-    cart.centerXDot <- updatedState[[2]]
-    cart.centerXDotDot <- updatedState[[3]]
-    cart.force <- updatedState[[4]]
-    pole.theta <- updatedState[[5]]
-    pole.thetaDot <- updatedState[[6]]
-    pole.thetaDotDot <- updatedState[[7]]
-    
-    createSceneFunc(scene.bottomLeftX,scene.bottomLeftY,scene.width,scene.height,
-                    main="Simulation of Inverted Pendulum - www.gekkoquant.com",xlab="",
-                    ylab="",xlim=c(-0.5*scene.width,0.5*scene.width),
-                    ylim=c(-0.5*scene.height,0.5*scene.height))
-    
-    createBoxFunc(track.x,track.y,track.limit*2,track.height,track.colour)
-    createBoxFunc(leftBuffer.x,leftBuffer.y,leftBuffer.width,leftBuffer.height,leftBuffer.colour)
-    createBoxFunc(rightBuffer.x,rightBuffer.y,rightBuffer.width,
-                  rightBuffer.height,rightBuffer.colour)
-    createBoxFunc(cart.centerX-0.5*cart.width,cart.centerY+0.5*cart.height,cart.width,cart.height,
-                  cart.colour)
-    drawPoleFunc(cart.centerX,cart.centerY,2*pole.length,pole.theta,pole.colour)
-    drawPendulum(cart.centerX,cart.centerY,2*pole.length,pole.theta,pendulum.radius,pendulum.colour)
-    
-  }
+  gridBot.PlotState <-function(updatedState){
+    updatedState[[2]]
+    }
   
  #Run
-  config <- newConfigNEAT(7,1,500,50)
-  poleSimulation <- newNEATSimulation(config, poleBalance.InitialState,
-                                      poleBalance.UpdatePoleState,
-                                      poleBalance.ConvertStateToNeuralNetInputs,
-                                      poleBalance.UpdateFitness,
-                                      poleBalance.CheckForTermination,
-                                      poleBalance.PlotState)
+  config <- newConfigNEAT(5,4,50,10)
+  gridBot <- newNEATSimulation(config, gridBot.InitialState,
+                               gridBot.UpdateState,
+                               gridBot.ConvertStateToNeuralNetInputs,
+                               gridBot.UpdateFitness,
+                               gridBot.CheckForTermination,
+                               gridBot.PlotState)
   
-  nMax <- 1 #Number of generations to run
+  nMax <- 2 #Number of generations to run
   for(i in seq(1,nMax)){
-    poleSimulation <- NEATSimulation.RunSingleGeneration(poleSimulation)
+    gridBotSimulation <- NEATSimulation.RunSingleGeneration(gridBot)
     #poleSimulation <- NEATSimulation.RunSingleGeneration(poleSimulation,T,"videos",
     #                                            "poleBalance",1/simulation.timestep)
   }
